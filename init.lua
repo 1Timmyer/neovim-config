7ddbeac8ff7ab9e2517591f07fd6725123ac14be
1Timmyer <tim@houbenmail.de>
2025-06-19 22:31:27 +0200

    rust

---
+++ init.lua
@@ -0,0 +1,204 @@
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set nu rnu")
+vim.cmd("set cursorline")
+vim.opt.guicursor = ""
+
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require('lazy').setup({
+  {'cohama/lexima.vim'},	
+  {'folke/tokyonight.nvim'},
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+  
+      configs.setup({
+          ensure_installed = { "c", "lua","python","go", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "markdown" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+  },
+  
+})
+
+-- Set colorscheme
+vim.opt.termguicolors = true
+vim.cmd.colorscheme('tokyonight-storm')
+
+---
+-- LSP setup
+---
+
+-- Reserve a space in the gutter
+-- This will avoid an annoying layout shift in the screen
+vim.opt.signcolumn = 'yes'
+
+-- Add cmp_nvim_lsp capabilities settings to lspconfig
+-- This should be executed before you configure any language server
+local lspconfig_defaults = require('lspconfig').util.default_config
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lspconfig_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
+-- This is where you enable features that only work
+-- if there is a language server active in the file
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  end,
+})
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  }
+})
+
+
+
+---
+-- Autocompletion config
+---
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  }),
+  snippet = {
+    expand = function(args)
+      vim.snippet.expand(args.body)
+    end,
+  },
+})
+
+
+require'lspconfig'.gopls.setup{}
+require("lspconfig").clangd.setup {
+  cmd = {
+      "clangd",
+      "--fallback-style=webkit"      
+  }
+}
+require'lspconfig'.pylsp.setup{}
+require'lspconfig'.tinymist.setup{
+ single_file_support = true,
+  settings = {
+    exportPdf = "onSave",
+  },
+}
+
+
+require("lspconfig").rust_analyzer.setup {
+    settings = {
+      ["rust-analyzer"] = {
+        check = {
+          command = "clippy", -- Empfohlen: Nutze Clippy für bessere Linting-Ergebnisse
+          allTargets = true,
+        },
+        procMacro = {
+          enable = true,
+        },
+        hover = {
+          actions = {
+            run = true,
+          },
+        },
+        diagnostics = {
+          enable = true,
+          disabled = {
+            -- Beispiel: Wenn du bestimmte Warnungen nicht sehen möchtest
+            -- "unresolved-macro-call",
+          },
+          experimental = {
+            enable = true,
+          },
+        },
+        cargo = {
+            loadOutDirsFromEnv = true,
+        },
+      },
+    },
+    -- Zusätzliche on_attach Konfigurationen, die nur für rust_analyzer gelten
+    -- (die allgemeinen on_attach-Einstellungen werden bereits aus config.lsp geladen)
+    on_attach = function(client, bufnr)
+        -- Hier könntest du rust-analyzer-spezifische Tastenkürzel hinzufügen
+        -- Beispiel: ":RustLsp" Kommando ist nützlich für rust-analyzer
+        vim.keymap.set("n", "<Leader>ra", "<cmd>RustLsp<CR>", { desc = "Rust Analyzer Commands" })
+    end,
+  }
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  upadate_in_insert = false,
+  severity_sort = true,
+})
+
+
+
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+
